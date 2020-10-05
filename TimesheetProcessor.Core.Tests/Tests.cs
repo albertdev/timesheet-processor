d2cbd44b003112bbfde02c8d7b0feb0a8f50d412
albertdev <albert.devs@gmail.com>
2020-10-06 01:25:31 +0200

    Implement full-week filter
    It's not perfect because no "zero" entries are made, but then the sheet
    writer code can perfectly well deal with it.

--- TimesheetProcessor.Core.Tests/Tests.cs
+++ TimesheetProcessor.Core.Tests/Tests.cs
@@ -224,7 +224,7 @@ namespace TimesheetProcessor.Core.Tests
             // Check that total hours are properly output
             Assert.Contains("\t'36:26:41", result);
         }
-        
+
         [Fact]
         public void TestManicTimeSheetWriter()
         {
@@ -244,10 +244,72 @@ namespace TimesheetProcessor.Core.Tests
                 new ManicTimesheetWriter().WriteTimesheet(timesheet, writer, true);
                 result = writer.ToString();
             }
-            
+
             Assert.Contains("Total", result);
             // Check that total hours are properly output
             Assert.Contains(",\"36.44\"", result);
+        }
+
+        [Fact]
+        public void TestFullWeekFilterFirstHalf()
+        {
+            var timesheet = new Builder()
+                .Day("2020-02-03").AddEntry("admin", "0:1:1").AddEntry("building, prod1", "0:5:30").AddEntry("work", "7:10:20")
+                .AndOnDay("2020-02-04").AddEntry("inventing", "0:7:1").AddEntry("building, prod2", "0:9:10").AddEntry("work", "6:58:43")
+                .AndOnDay("2020-02-05").AddEntry("admin", "0:0:1").AddEntry("work", "7:39:33")
+                .ToTimesheet();
+
+            timesheet = new FullWeekFilter().Filter(timesheet);
+
+            Assert.Equal(new DateTime(2020, 02, 03), timesheet.Days[0].Day);
+            Assert.Equal(new DateTime(2020, 02, 04), timesheet.Days[1].Day);
+            Assert.Equal(new DateTime(2020, 02, 05), timesheet.Days[2].Day);
+            Assert.Equal(new DateTime(2020, 02, 06), timesheet.Days[3].Day);
+            Assert.Equal(new DateTime(2020, 02, 07), timesheet.Days[4].Day);
+            Assert.Equal(new DateTime(2020, 02, 08), timesheet.Days[5].Day);
+            Assert.Equal(new DateTime(2020, 02, 09), timesheet.Days[6].Day);
+        }
+
+        [Fact]
+        public void TestFullWeekFilterLastHalf()
+        {
+            var timesheet = new Builder()
+                .Day("2020-02-06").AddEntry("admin", "0:1:1").AddEntry("building, prod1", "0:5:30").AddEntry("work", "7:10:20")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1").AddEntry("work", "6:05:17")
+                .AndOnDay("2020-02-08").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
+
+            timesheet = new FullWeekFilter().Filter(timesheet);
+
+            Assert.Equal(new DateTime(2020, 02, 03), timesheet.Days[0].Day);
+            Assert.Equal(new DateTime(2020, 02, 04), timesheet.Days[1].Day);
+            Assert.Equal(new DateTime(2020, 02, 05), timesheet.Days[2].Day);
+            Assert.Equal(new DateTime(2020, 02, 06), timesheet.Days[3].Day);
+            Assert.Equal(new DateTime(2020, 02, 07), timesheet.Days[4].Day);
+            Assert.Equal(new DateTime(2020, 02, 08), timesheet.Days[5].Day);
+            Assert.Equal(new DateTime(2020, 02, 09), timesheet.Days[6].Day);
+        }
+
+        [Fact]
+        public void TestFullWeekFilterRandomDayMissing()
+        {
+            var timesheet = new Builder()
+                .Day("2020-02-03").AddEntry("admin", "0:1:1").AddEntry("building, prod1", "0:5:30").AddEntry("work", "7:10:20")
+                .AndOnDay("2020-02-06").AddEntry("admin", "0:0:1").AddEntry("work", "8:10:01")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1").AddEntry("work", "6:05:17")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
+
+            timesheet = new FullWeekFilter().Filter(timesheet);
+
+            Assert.Equal(new DateTime(2020, 02, 03), timesheet.Days[0].Day);
+            Assert.Equal(new DateTime(2020, 02, 04), timesheet.Days[1].Day);
+            Assert.Equal(new DateTime(2020, 02, 05), timesheet.Days[2].Day);
+            Assert.Equal(new DateTime(2020, 02, 06), timesheet.Days[3].Day);
+            Assert.Equal(new DateTime(2020, 02, 07), timesheet.Days[4].Day);
+            Assert.Equal(new DateTime(2020, 02, 08), timesheet.Days[5].Day);
+            Assert.Equal(new DateTime(2020, 02, 09), timesheet.Days[6].Day);
         }
     }
 }
--- TimesheetProcessor.Core/Filter/FullWeekFilter.cs
+++ TimesheetProcessor.Core/Filter/FullWeekFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using TimesheetProcessor.Core.Dto;
 
@@ -12,16 +13,55 @@ namespace TimesheetProcessor.Core.Filter
     {
         public Timesheet Filter(Timesheet original)
         {
-            // No-op
-            // if (original.Days.Count == 7)
-            // {
+            if (original.Days.Count == 7)
+            {
                 return original;
-            // }
+            }
             
-            // var result = (Timesheet)original.Clone();
+            var result = (Timesheet)original.Clone();
 
-            // var firstDay = result.Days[0];
-            // if (firstDay.Day.DayOfWeek == DayOfWeek.Monday)
+            var firstDayOfSheet = result.Days.Single(x => x.Day.Equals(result.Days.Min(y => y.Day)));
+            int daysToPrepend = DaysToPrepend(firstDayOfSheet.Day.DayOfWeek);
+            
+            DateTime firstDayOfWeek = firstDayOfSheet.Day.AddDays(-daysToPrepend);
+            DateTime lastDayOfWeek = firstDayOfWeek.AddDays(6);
+            var currentDay = firstDayOfWeek;
+            int insertPos = 0;
+            while (currentDay <= lastDayOfWeek)
+            {
+                if (! result.Days.Any(x => x.Day.Equals(currentDay)))
+                {
+                    DayEntry newDay = new DayEntry() {Day = currentDay};
+                    result.Days.Insert(insertPos, newDay);
+                }
+                insertPos++;
+                currentDay = currentDay.AddDays(1);
+            }
+
+            return result;
+        }
+
+        private static int DaysToPrepend(DayOfWeek weekDay)
+        {
+            switch (weekDay)
+            {
+                case DayOfWeek.Monday:
+                    return 0;
+                case DayOfWeek.Tuesday:
+                    return 1;
+                case DayOfWeek.Wednesday:
+                    return 2;
+                case DayOfWeek.Thursday:
+                    return 3;
+                case DayOfWeek.Friday:
+                    return 4;
+                case DayOfWeek.Saturday:
+                    return 5;
+                case DayOfWeek.Sunday:
+                    return 6;
+                default:
+                    throw new Exception("The calendar is broken again");
+            }
         }
     }
 }
