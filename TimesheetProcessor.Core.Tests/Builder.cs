d4c73b0bf11f33dc3c22daeddfe34e460ca37297
albertdev <albert.devs@gmail.com>
2020-10-05 20:26:52 +0200

    Tests: add a timesheet builder so tests are independent from parsing

---
+++ TimesheetProcessor.Core.Tests/Builder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TimesheetProcessor.Core.Dto;
+
+namespace TimesheetProcessor.Core.Tests
+{
+    public class Builder
+    {
+        private Timesheet _sheet = new Timesheet();
+        private Dictionary<string, TagDetails> _tagMap = new Dictionary<string, TagDetails>();
+        
+        public Builder()
+        {
+        }
+
+        public EntryBuilder Day(string date)
+        {
+            return new EntryBuilder(this, date);
+        }
+        
+        public class EntryBuilder
+        {
+            private readonly Builder _builder;
+            private DayEntry _dayEntry;
+
+            public EntryBuilder(Builder builder, string dateValue)
+            {
+                _builder = builder;
+                var date = DateTime.ParseExact(dateValue, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                _dayEntry = new DayEntry
+                {
+                    Day = date
+                };
+                _builder._sheet.Days.Add(_dayEntry);
+            }
+
+            public EntryBuilder AddEntry(string tag, string timeSpent)
+            {
+                return AddEntry(tag, timeSpent, false);
+            }
+
+            public EntryBuilder AddReadOnlyEntry(string tag, string timeSpent)
+            {
+                return AddEntry(tag, timeSpent, true);
+            }
+
+            private EntryBuilder AddEntry(string tag, string timeSpent, bool readOnly)
+            {
+                TagDetails tagDetails;
+                if (_builder._tagMap.ContainsKey(tag))
+                {
+                    tagDetails = _builder._tagMap[tag];
+                }
+                else
+                {
+                    int separatorIndex = tag.IndexOf(",", StringComparison.InvariantCulture);
+                    tagDetails = new TagDetails
+                    {
+                        Tag1 = separatorIndex == -1 ? tag : tag.Substring(0, separatorIndex),
+                        Tag2 = separatorIndex == -1 ? null : tag.Substring(separatorIndex + 1).TrimStart()
+                    };
+                    _builder._tagMap[tagDetails.TagId] = tagDetails;
+                }
+                var timeSpentParsed = TimeSpan.ParseExact(timeSpent, "h\\:m\\:s", CultureInfo.InvariantCulture);
+                var entry = new TimeEntry(timeSpentParsed, _dayEntry, tagDetails, readOnly);
+                _dayEntry.Entries.Add(entry);
+                tagDetails.Entries.Add(entry);
+                return this;
+            }
+
+            public EntryBuilder AndOnDay(string date)
+            {
+                return new EntryBuilder(_builder, date);
+            }
+
+            public Timesheet ToTimesheet()
+            {
+                foreach (var tag in _builder._tagMap.Values)
+                {
+                    _builder._sheet.Tags.Add(tag);
+                }
+                return _builder._sheet;
+            }
+        }
+    }
+}
--- TimesheetProcessor.Core.Tests/Tests.cs
+++ TimesheetProcessor.Core.Tests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using TimesheetProcessor.Core.Dto;
 using Xunit;
 using TimesheetProcessor.Core.Dto;
@@ -10,7 +11,7 @@ namespace TimesheetProcessor.Core.Tests
     public class Tests
     {
         [Fact]
-        public void TestNonPadded()
+        public void TestParseFullWeek()
         {
             Timesheet test;
             ManicTimeParser parser = new ManicTimeParser();
@@ -24,7 +25,7 @@ namespace TimesheetProcessor.Core.Tests
         }
         
         [Fact]
-        public void TestHalfWeekOK()
+        public void TestParseHalfWeek()
         {
             Timesheet test;
             ManicTimeParser parser = new ManicTimeParser();
@@ -38,7 +39,7 @@ namespace TimesheetProcessor.Core.Tests
         }
         
         [Fact]
-        public void TestFailsIfMoreThanOneWeek()
+        public void TestParseFailsIfMoreThanOneWeek()
         {
             Timesheet test;
             ManicTimeParser parser = new ManicTimeParser();
@@ -51,57 +52,71 @@ namespace TimesheetProcessor.Core.Tests
         [Fact]
         public void TestExpectedTimeSpent()
         {
-            Timesheet test;
-            ManicTimeParser parser = new ManicTimeParser();
-            using (var stream = new StreamReader("Testfiles/Timesheet_nonpad.tsv"))
-            {
-                test = parser.ParseTimesheet(stream);
-            }
-            Assert.Equal(TimeSpan.FromHours(39), test.ExpectedHoursSpent);
+            var timesheet = new Builder()
+                .Day("2020-02-03").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-04").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-05").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-06").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-08").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
+            
+            Assert.Equal(TimeSpan.FromHours(39), timesheet.ExpectedHoursSpent);
         }
         
         [Fact]
         public void TestExpectedTimeSpentHalfWeek()
         {
-            Timesheet test;
-            ManicTimeParser parser = new ManicTimeParser();
-            using (var stream = new StreamReader("Testfiles/Timesheet_halfweek.tsv"))
-            {
-                test = parser.ParseTimesheet(stream);
-            }
-            Assert.Equal(TimeSpan.FromHours(8 + 8 + 7), test.ExpectedHoursSpent);
+            // Wednesday, Thursday, Friday + weekend
+            var timesheet = new Builder()
+                .Day("2020-02-05").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-06").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-08").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
+            
+            Assert.Equal(TimeSpan.FromHours(8 + 8 + 7), timesheet.ExpectedHoursSpent);
         }
         
         [Fact]
         public void TestReadOnlyTimeSpent()
         {
-            Timesheet test;
-            ManicTimeParser parser = new ManicTimeParser();
-            using (var stream = new StreamReader("Testfiles/Timesheet_nonpad.tsv"))
-            {
-                test = parser.ParseTimesheet(stream);
-            }
-            Assert.Equal(TimeSpan.FromHours(16), test.TotalTimeSpentWithReadonlyFlag);
+            var timesheet = new Builder()
+                .Day("2020-02-03").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-04").AddReadOnlyEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-05").AddReadOnlyEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-06").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-08").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
+            Assert.Equal(TimeSpan.FromSeconds(2), timesheet.TotalTimeSpentWithReadonlyFlag);
         }
         
         [Fact]
         public void TestRoundUpToDeciHourFilter()
         {
-            Timesheet test;
-            ManicTimeParser parser = new ManicTimeParser();
-            using (var stream = new StreamReader("Testfiles/Timesheet_nonpad.tsv"))
-            {
-                test = parser.ParseTimesheet(stream);
-            }
+            var timesheet = new Builder()
+                .Day("2020-02-03").AddEntry("admin", "0:1:1").AddEntry("building, prod1", "0:5:30")
+                .AndOnDay("2020-02-04").AddEntry("inventing", "0:7:1").AddEntry("building, prod2", "0:9:0")
+                .AndOnDay("2020-02-05").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-06").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-07").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-08").AddEntry("admin", "0:0:1")
+                .AndOnDay("2020-02-09").AddEntry("admin", "0:0:1")
+                .ToTimesheet();
             
             var filter = new RoundUpToDeciHourFilter();
-            test = filter.Filter(test);
+            timesheet = filter.Filter(timesheet);
             
-            Assert.Equal(new TimeSpan(1, 18, 0), test.Days[0].Entries[0].TimeSpent);
-            Assert.Equal(new TimeSpan(5, 54, 0), test.Days[0].TotalTimeSpent);
-            Assert.Equal(new TimeSpan(4, 36, 0), test.Tags[4].TotalTimeSpent);
+            Assert.Equal(new TimeSpan(0, 6, 0), timesheet.Days[0].Entries[0].TimeSpent);
+            Assert.Equal(new TimeSpan(0, 12, 0), timesheet.Days[0].TotalTimeSpent);
+            Assert.Equal(new TimeSpan(0, 24, 0), timesheet.Days[1].TotalTimeSpent);
+            Assert.Equal(new TimeSpan(0, 36, 0), timesheet.Tags.First(x => x.TagId == "admin").TotalTimeSpent);
             
-            Assert.Equal(new TimeSpan(31, 24, 0), test.TotalTimeSpent);
+            Assert.Equal(new TimeSpan(1, 6, 0), timesheet.TotalTimeSpent);
         }
     }
 }
