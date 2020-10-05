70f1f5cad8ecd93dfce91ee0a11cdd5c9649d034
albertdev <albert.devs@gmail.com>
2020-10-06 00:16:58 +0200

    Rework scaling logic, OK timesheets are only minimally modified
    The scaling logic seems to work a whole lot better if the scaling factor
    is calculated on a "round to nearest (but never zero)" basis.
    
    Also, timesheets which have been corrected should not be scaled again,
    at best they should be rounded to the nearest numbers. They can then
    pass through the FullWeekFilter (to be implemented) so that they are
    ready for entry.

--- TimesheetProcessor.Program.ClipboardInput/Program.cs
+++ TimesheetProcessor.Program.ClipboardInput/Program.cs
@@ -29,40 +29,72 @@ namespace TimesheetProcessor.Program.ClipboardInput
                 sheet = new ManicTimeParser().ParseTimesheet(reader);
             }
             
-            var roundedUp = new RoundUpToDeciHourFilter().Filter(sheet);
+            var roundedUp = new RoundToNearestDeciHourFilter().Filter(sheet);
 
-            double flexibleHours = (roundedUp.ExpectedHoursSpent - roundedUp.TotalTimeSpentWithReadonlyFlag).TotalSeconds;
-            double hoursUpForScaling = (roundedUp.TotalTimeSpent - roundedUp.TotalTimeSpentWithReadonlyFlag).TotalSeconds;
-            var scalingFactor = flexibleHours / hoursUpForScaling;
+            double expectedHoursToScale = (roundedUp.ExpectedHoursSpent - roundedUp.TotalTimeSpentWithReadonlyFlag).TotalSeconds;
+            double actualHoursToScale = (roundedUp.TotalTimeSpent - roundedUp.TotalTimeSpentWithReadonlyFlag).TotalSeconds;
+            var scalingFactor = expectedHoursToScale / actualHoursToScale;
             
-            var scaledTimesheet = new ScaleTimesheetFilter(scalingFactor).Filter(sheet);
-            scaledTimesheet = new RoundToNearestDeciHourFilter().Filter(scaledTimesheet);
-            
-            var difference = new SubtractTimesheetFilter(sheet).Filter(scaledTimesheet);
+            // Do not scale timesheet if there's only a one-hour difference (or even overtime). By that point it's better to review things manually.
+            bool shouldScale = (actualHoursToScale < (expectedHoursToScale - 1));
 
-            string result;
+            Timesheet result = sheet;
+            Timesheet difference = null;
+            if (shouldScale)
+            {
+                result = new ScaleTimesheetFilter(scalingFactor).Filter(sheet);
+            }
+
+            // Even if we don't scale we can better make sure the timesheet is rounded to 6 minute blocks
+            result = new RoundToNearestDeciHourFilter().Filter(result);
+
+            if (shouldScale) 
+            {
+                difference = new SubtractTimesheetFilter(sheet).Filter(result);
+                difference = new FullWeekFilter().Filter(difference);
+            }
+            
+            // Make sure timesheet contains all 7 days of the week for importing it in other time tracking tool
+            result = new FullWeekFilter().Filter(result);
+
+            string resultOutput;
             using (var writer = new StringWriter())
             {
                 writer.WriteLine("Processed");
                 writer.Write("Week:\t");
                 writer.WriteLine(sheet.WeekNumber);
                 writer.Write("Scale factor:\t");
-                writer.WriteLine(scalingFactor);
-                writer.WriteLine();
-                writer.WriteLine();
-                
-                // Should actually be a comma-separated one if we want to use this directly with helper scripts, but anyway
-                new TabBasedTimesheetWriter().WriteTimesheet(scaledTimesheet, writer, true);
 
+                if (shouldScale)
+                {
+                    writer.WriteLine(scalingFactor);
+                }
+                else
+                {
+                    writer.WriteLine("None");
+                }
                 writer.WriteLine();
                 writer.WriteLine();
-                new TabBasedTimesheetWriter().WriteTimesheet(difference, writer, false);
+
+                new TabBasedTimesheetWriter().WriteTimesheet(result, writer, true);
+                    
+                if (shouldScale)
+                {
+                    // Include 'difference' timesheet for applying manual corrections in ManicTime
+                    writer.WriteLine();
+                    writer.WriteLine();
+                    new TabBasedTimesheetWriter().WriteTimesheet(difference, writer, false);
+                }
+                writer.WriteLine();
+                writer.WriteLine();
+                // Print (potentially corrected) timesheet once more, but now in format for import in external time tracking tool
+                new ManicTimesheetWriter().WriteTimesheet(result, writer, true);
                 
                 writer.Flush();
-                result = writer.ToString();
+                resultOutput = writer.ToString();
             }
 
-            Clipboard.SetText(result);
+            Clipboard.SetText(resultOutput);
             Console.Out.WriteLine("Processing complete");
             Console.ReadKey();
         }
